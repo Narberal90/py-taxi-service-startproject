6caa8a66012de20f1aedda6c37ca7cc6b325dc05
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-09-05 22:00:30 +0300

    improved models

--- taxi/models.py
+++ taxi/models.py
@@ -7,12 +7,37 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=250, unique=True)
     country = models.CharField(max_length=250)
 
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self) -> str:
+        return f"Name: {self.name} country: {self.country}"
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=250, unique=True)
 
+    class Meta:
+        ordering = ("username",)
+
+    def __str__(self) -> str:
+        return f"{self.license_number}"
+
 
 class Car(models.Model):
     model = models.CharField(max_length=250)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="manufacturer")
-    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+        related_name="cars"
+    )
+    drivers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="cars"
+    )
+
+    class Meta:
+        ordering = ("model", "manufacturer",)
+
+    def __str__(self) -> str:
+        return f"{self.model} {self.manufacturer.name}"
